267d6b9615ef832d0504989d4534636213de3cb6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2026-03-19 08:22:57 +0100

    Update KeyboardAug.py

--- PEC_AI_Classifier/KeyboardAug.py
+++ PEC_AI_Classifier/KeyboardAug.py
@@ -1,1 +1,10 @@
 # KeyboardAug (errori battitura, comune in PEC veloci)
+
+keyboard_aug = nac.KeyboardAug(action="substitute", aug_char_p=0.05, aug_max=3)
+augmented_keyboard = keyboard_aug.augment(pec_samples, n=3)
+
+print("Keyboard Augmented:", augmented_keyboard)
+
+# Output:​
+
+# ['Fattura Eletrronica n.123...', 'Fattura Elettronica n.12;...', 'Fatutra Elettronica n.123...']
